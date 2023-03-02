ebd1b79c772fba9ca34d8cd491a2242f7380023c
Jean Parpaillon <jean.parpaillon@free.fr>
2023-03-02 17:42:15 +0100

    mix new aloha_delta_updater

---
+++ lib/aloha_delta_updater.ex
@@ -0,0 +1,18 @@
+defmodule AlohaDeltaUpdater do
+  @moduledoc """
+  Documentation for `AlohaDeltaUpdater`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlohaDeltaUpdater.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aloha_delta_updater_test.exs
@@ -0,0 +1,8 @@
+defmodule AlohaDeltaUpdaterTest do
+  use ExUnit.Case
+  doctest AlohaDeltaUpdater
+
+  test "greets the world" do
+    assert AlohaDeltaUpdater.hello() == :world
+  end
+end
